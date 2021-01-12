f8c63db0ae1894f9fc20fd78fdf283fe39e31eba
Shobhit Jain <jain.shobhit01@gmail.com>
2021-01-11 20:12:47 -0800

    Added Data Preprocessing and baseline XGBoost

---
+++ DataPreprocessing.py
@@ -0,0 +1,21 @@
+monthly_data = train.groupby(['shop_id','item_id','date_block_num']).agg({'item_cnt_day':'sum','item_price':'mean'}).reset_index(); # groupby automatically removes the column not mentioned, reset_index doesn't make the groupby columns as index
+monthly_data = monthly_data.rename(columns={'date_block_num':'month_block_num','item_price':'mean_item_price','item_cnt_day':'item_cnt_month'});
+# Combining all test points
+monthly_data = pd.merge(test,monthly_data,on=['shop_id','item_id'],how='left') 
+# Filling NaN
+monthly_data=monthly_data.fillna(0)
+# Sorting
+monthly_data.sort_values(by=['shop_id','item_id','month_block_num'])
+# Clipping
+monthly_data['item_cnt_month'] = monthly_data['item_cnt_month'].clip(0,20)
+# Combining all info in monthly_data table
+monthly_data = pd.merge(monthly_data,items,on=['item_id'],how='inner')
+monthly_data = pd.merge(monthly_data,categories,on=['item_category_id'],how='inner')
+monthly_data = pd.merge(shops,monthly_data,on=['shop_id'],how='inner')
+
+
+# Doing the same stuff for test
+test_modified = pd.merge(test,items, on = ['item_id'],how='inner')
+test_modified['month_block_num'] = 33
+test_modified = test_modified.drop(['item_name'], axis=1)
+test_modified = test_modified.set_index('ID')
--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # TimeSeriesKaggle
 
-# Baseline Case
-In this model, we use last month's sales as the prediction for this month's value (Can be thought as Moving average with window size of 1).
-Steps involved include:
-1. Aggregating daily data to monthly data.
-2. Joining test data and train data to get all possible pairs.
-3. Filling null values, clipping monthly sales to [0,20] and extracting only last month sales.
+This repository is for the Kaggle competition on forecasting the total sales for every product and store in the next month for one of the largest Russian software firms - 1C Company.
+ 
+Link to the competition: https://www.kaggle.com/c/competitive-data-science-predict-future-sales/overview
